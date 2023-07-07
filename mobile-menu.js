15b7efe88d256ba359f39068b7463d9932101618
Binyam Y. Olango <125895809+binyamolango@users.noreply.github.com>
2023-07-07 13:05:52 +0300

    Fix linter

--- mobile-menu.js
+++ mobile-menu.js
@@ -4,13 +4,13 @@ const menu = document.getElementById('menu');
 const menuNav = document.getElementById('menu-nav-list');
 
 function openMenu() {
-    hamBtn.classList.toggle('hid');
-    menu.classList.toggle('hid');
+  hamBtn.classList.toggle('hid');
+  menu.classList.toggle('hid');
 }
 
 function closeMenu() {
-    hamBtn.classList.toggle('hid');
-    menu.classList.toggle('hid');
+  hamBtn.classList.toggle('hid');
+  menu.classList.toggle('hid');
 }
 
 hamBtn.addEventListener('click', openMenu);
